77985ca920c61990e04533c526eecab9cdae622c
MinZexing <zexing1908@gmail.com>
2025-03-30 23:26:39 +1100

    build overall website layout and pages in static

--- app/layout.js
+++ app/layout.js
@@ -25,14 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <Header>
-          <Logo />
-          <Navigation />
-        </Header>
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
