9128658e3d9ddf919389becfe6d88e80b13a6c0b
MinZexing <zexing1908@gmail.com>
2025-04-19 02:57:26 +1000

    implement filter feature in the cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -4,20 +4,18 @@ import { getCabins } from "../_lib/data-service";
 import CabinList from "../_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 //In seconds
-export const revalidate = 3600;
+//revalidate is only for static page
+// export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
-  // CHANGE
-  console.log("Starting");
-  const cabins = await getCabins();
-
-  console.log(cabins);
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
 
   return (
     <div>
@@ -32,8 +30,13 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
